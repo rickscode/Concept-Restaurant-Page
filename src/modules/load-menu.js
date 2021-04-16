8fab1d6386d5892aa68514abd755c37937e89bec
“Ricky <grickyh@gmail.com>
2021-04-16 07:02:03 +0100

    morning check

--- src/modules/load-menu.js
+++ src/modules/load-menu.js
@@ -3,6 +3,8 @@ function loadMenuPage()
 {
     const content = document.getElementById("content");
     const menu = document.createElement("div");
+    const pizzaMenu = document.createElement("img");
+    pizzaMenu.setAttribute("src", "https://i.imgur.com/d36stwa.png");
     menu.id = "menu-page";
     menu.textContent = 'PIZZA :)';
     content.appendChild(menu);
