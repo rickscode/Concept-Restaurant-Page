732dc96a32120e7b154e1fec9445569b9caa17ea
“Ricky <grickyh@gmail.com>
2021-04-17 09:39:35 +0100

    contact number added

--- src/modules/load-contact.js
+++ src/modules/load-contact.js
@@ -4,7 +4,7 @@ function loadContactPage()
     const content = document.getElementById("content");
     const number = document.createElement("div");
     number.id = "contact-page";
-    number.textContent = '0021839473840417';
+    number.textContent = '002183947';
     content.appendChild(number);
 }
 
